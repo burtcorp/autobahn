3ac1662dc3a9763e88752f075393207546e81057
David Tollmyr <david@burtcorp.com>
2012-06-14 15:49:09 +0200

    Added msgpack and bson encoders

--- lib/autobahn/encoder.rb
+++ lib/autobahn/encoder.rb
@@ -25,4 +25,34 @@ module Autobahn
     end
   rescue LoadError
   end
+
+  begin
+    require 'msgpack'
+
+    class MsgPackEncoder
+      def encode(obj)
+        MessagePack.pack(obj)
+      end
+
+      def decode(str)
+        MessagePack.unpack(str.force_encoding(Encoding::ASCII_8BIT))
+      end
+    end
+  rescue LoadError
+  end
+
+  begin
+    require 'bson'
+
+    class BsonEncoder
+      def encode(obj)
+        BSON.serialize(obj).to_s
+      end
+
+      def decode(str)
+        BSON.deserialize(str)
+      end
+    end
+  rescue LoadError
+  end
 end
