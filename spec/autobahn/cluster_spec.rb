2d6b4a24fba5faabfa7117804912821f07c9c1eb
Gustav Munkby <grddev@gmail.com>
2014-05-23 13:51:06 +0200

    Clarified tests for default API URI
    Explicitly test the caching after first request.

--- spec/autobahn/cluster_spec.rb
+++ spec/autobahn/cluster_spec.rb
@@ -35,20 +35,36 @@ module Autobahn
           Cluster.new(nil)
         end
 
+        let :stub_legacy_request do
+          stub_request(:get, "http://guest:guest@localhost:55672/api/overview")
+        end
+
+        let :stub_modern_request do
+          stub_request(:get, "http://guest:guest@localhost:15672/api/overview")
+        end
+
         it 'connects to localhost:15672 for testing purposes' do
-          stub_request(:get, "http://guest:guest@localhost:15672/api/overview").to_return(
-            :body => "{}")
+          stub_modern_request.to_return(:body => '{}')
 
           cluster.overview.should == {}
         end
 
-        it 'reverts to legacy localhost:55672 if connection fails' do
-          stub_request(:get, "http://guest:guest@localhost:15672/api/overview").to_raise(
-            Errno::ECONNREFUSED)
-          stub_request(:get, "http://guest:guest@localhost:55672/api/overview").to_return(
-            :body => "{}")
+        it 'reverts to legacy localhost:55672 when the connection fails' do
+          stub_modern_request.to_raise(Errno::ECONNREFUSED)
+          stub_legacy_request.to_return(:body => '{}')
 
           cluster.overview.should == {}
+        end
+
+        it 'caches the URI after the first request' do
+          stub_modern_request.to_raise(Errno::ECONNREFUSED)
+          stub_modern_request.to_return(:body => '{}')
+
+          stub_legacy_request.to_return(:body => '{}')
+          stub_legacy_request.to_raise(Errno::ECONNREFUSED)
+
+          cluster.overview.should == {}
+          expect { cluster.overview }.to raise_error(Errno::ECONNREFUSED)
         end
       end
     end
